e4ac215677ea13cddded4d6965da75c5fea6bf16
Matthew Paul <mattgpaul@gmail.com>
2023-12-10 16:15:44 -0700

    project initialization

---
+++ src/app/led.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <avr/io.h>
+#include <util/delay.h>
+
+class Register{
+    private:
+        char _port_char;
+        int _port_num;
+        std::string _name;
+    public:
+
+}
