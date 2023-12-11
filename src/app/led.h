3937e3e19a9e59262519b3cb1c845538a26c2115
Matthew Paul <mattgpaul@gmail.com>
2023-12-10 17:29:04 -0700

    first attempt at abstraction

--- src/app/led.h
+++ src/app/led.h
@@ -4,11 +4,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-class Register{
-    private:
-        char _port_char;
-        int _port_num;
-        std::string _name;
-    public:
+class PortDevice{
+private:
+    char _port_char;
+    int _port_num;
+    bool _direction;
+    std::string _name;
 
-}
+public:
+    PortDevice(char port_char, int port_num, bool data_direction, std::string device_name);
+    char get_port_char();
+    int get_port_num();
+    bool get_data_direction();
+    std::string get_device_name();
+};
